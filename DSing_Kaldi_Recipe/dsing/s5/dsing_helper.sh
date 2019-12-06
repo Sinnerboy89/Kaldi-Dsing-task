64f64359f54308ee0630cf17b84f04875126fd92
Chris B <chrisb@cereproc.com>
2019-12-06 16:17:48 +0000

    Remove duplicate

--- DSing_Kaldi_Recipe/dsing/s5/dsing_helper.sh
+++ DSing_Kaldi_Recipe/dsing/s5/dsing_helper.sh
@@ -1,5 +1,7 @@
 # fresh start
-rm -rv data
+rm -rvf data
+rm -rvf exp
+rm -rvf mfcc
 
 ./run.sh
 
@@ -14,4 +16,34 @@ chmod +x steps/make_mfcc.sh
 chmod +x utils/validate_text.pl
 chmod +x utils/create_data_link.pl
 chmod +x utils/split_scp.pl
-chmod +x /home/chrisb/Kaldi-Dsing-task/Dsing_Kaldi_Recipe/dsing/s5/utils/run.pl
+
+# remove hard run.pl and replace with soft-link to parallel version
+cd utils
+rm -v run.pl
+ln -s parallel/run.pl run.pl
+
+# more permissions
+chmod +x steps/compute_cmvn_stats.sh
+chmod +x steps/train_mono.sh
+chmod +x utils/data/get_utt2dur.sh
+chmod +x utils/data/internal/modify_speaker_info.py
+chmod +x utils/filter_scps.pl
+chmod +x steps/diagnostic/analyze_alignments.sh
+chmod +x utils/summarize_warnings.pl
+chmod +x steps/info/gmm_dir_info.pl
+chmod +x steps/align_si.sh
+chmod +x steps/decode_fmllr.sh
+
+# turn on "allow_uneven_split" in train_mono.sh or else it complains
+
+# more permissions
+chmod +x steps/diagnostic/analyze_alignments.sh
+chmod +x utils/lang/check_phones_compatible.sh
+chmod +x steps/train_deltas.sh
+chmod +x steps/train_lda_mllt.sh
+chmod +x steps/train_sat.sh
+chmod +x steps/decode.sh
+chmod +x steps/cleanup/clean_and_segment_data.sh
+chmod +x local/chain/run_tdnn_1d.sh
+chmod +x steps/diagnostic/analyze_lats.sh
+chmod +x steps/scoring/score_kaldi_wer.sh
